6fc03ea439fdf44b0a45e5c9fc1b1264e118480d
R. Cem Kahveci <cem.kahveci@siemens.com>
2023-02-02 10:16:59 +0300

    Add project files.

---
+++ AdoNetWinForm/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace AdoNetWinForm
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblUserType = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbResultViewer = new System.Windows.Forms.ListBox();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.cbUserType = new System.Windows.Forms.ComboBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.lblTotalLabel = new System.Windows.Forms.Label();
+            this.lblTotalCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 18);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(12, 49);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(72, 16);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(12, 77);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(76, 16);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "User Name";
+            // 
+            // lblUserType
+            // 
+            this.lblUserType.AutoSize = true;
+            this.lblUserType.Location = new System.Drawing.Point(12, 106);
+            this.lblUserType.Name = "lblUserType";
+            this.lblUserType.Size = new System.Drawing.Size(71, 16);
+            this.lblUserType.TabIndex = 3;
+            this.lblUserType.Text = "User Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gender";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(124, 15);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(346, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(124, 46);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(346, 22);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(124, 74);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(346, 22);
+            this.txtUserName.TabIndex = 8;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(124, 133);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(346, 22);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // lbResultViewer
+            // 
+            this.lbResultViewer.FormattingEnabled = true;
+            this.lbResultViewer.ItemHeight = 16;
+            this.lbResultViewer.Location = new System.Drawing.Point(11, 213);
+            this.lbResultViewer.Name = "lbResultViewer";
+            this.lbResultViewer.Size = new System.Drawing.Size(459, 212);
+            this.lbResultViewer.TabIndex = 14;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(329, 163);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(141, 44);
+            this.btnAddUser.TabIndex = 17;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            // 
+            // cbUserType
+            // 
+            this.cbUserType.FormattingEnabled = true;
+            this.cbUserType.Location = new System.Drawing.Point(124, 103);
+            this.cbUserType.Name = "cbUserType";
+            this.cbUserType.Size = new System.Drawing.Size(346, 24);
+            this.cbUserType.TabIndex = 18;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(124, 161);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(58, 20);
+            this.rbMale.TabIndex = 20;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(188, 161);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(74, 20);
+            this.rbFemale.TabIndex = 21;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(11, 436);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(213, 23);
+            this.btnShowAll.TabIndex = 13;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(253, 436);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(217, 23);
+            this.btnClearAll.TabIndex = 22;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalLabel
+            // 
+            this.lblTotalLabel.AutoSize = true;
+            this.lblTotalLabel.Location = new System.Drawing.Point(12, 191);
+            this.lblTotalLabel.Name = "lblTotalLabel";
+            this.lblTotalLabel.Size = new System.Drawing.Size(82, 16);
+            this.lblTotalLabel.TabIndex = 23;
+            this.lblTotalLabel.Text = "Total Entries";
+            // 
+            // lblTotalCounter
+            // 
+            this.lblTotalCounter.AutoSize = true;
+            this.lblTotalCounter.Location = new System.Drawing.Point(121, 191);
+            this.lblTotalCounter.Name = "lblTotalCounter";
+            this.lblTotalCounter.Size = new System.Drawing.Size(18, 20);
+            this.lblTotalCounter.TabIndex = 24;
+            this.lblTotalCounter.Text = "0";
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 469);
+            this.Controls.Add(this.lblTotalCounter);
+            this.Controls.Add(this.lblTotalLabel);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.cbUserType);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.lbResultViewer);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblUserType);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Name = "UserForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clear All";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblUserType;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.ListBox lbResultViewer;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.ComboBox cbUserType;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Label lblTotalLabel;
+        private System.Windows.Forms.Label lblTotalCounter;
+    }
+}
+
